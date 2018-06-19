76235011016d8feb2c32b7fc7794d8be53d65a92
Chintan Shah <chintan.avs@gmail.com>
2018-06-19 11:55:11 +0530

    Connecting and Disconnecting using event handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,34 @@
 const path=require('path');
 const express=require('express');
-
-var app=express();
-var port=process.env.PORT || 3000;
+const socketIO=require('socket.io');
+const http=require('http')
 
 //path.join is used to omit the relative path
-publicPath=path.join(__dirname,'../public');
+const publicPath=path.join(__dirname,'../public');
+var port=process.env.PORT || 3000;
+
+var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 
 //use to define the middleware
 app.use(express.static(publicPath));
 
+//used to set up a new connection
+io.on('connection',(socket)=>{
+  console.log('new user connected');
 
-app.listen(port,()=>{
-  console.log(`app is started at port ${port}`);
+socket.on('disconnect',()=>{
+  console.log('User was disconnected');
+})
+
+
+});
+
+
+
+
+server.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
 });
